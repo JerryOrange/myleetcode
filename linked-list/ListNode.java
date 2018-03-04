310a3f5b3c56489ed91fd0eecbbb2b197e24fdc7
JerryOrange <563146556@qq.com>
2018-03-04 16:28:02 +0800

    Definition for singly-linked list

---
+++ linked-list/ListNode.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ *
+ * Created by Jiacheng on 2018/3/4.
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+    }
+
+}
