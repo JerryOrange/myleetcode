85073fba0063df8d5b57de0c2cae845dcb923a54
JerryOrange <563146556@qq.com>
2018-06-20 10:13:48 +0800

    调整数组顺序使奇数位于偶数前面

---
+++ offer-algorithm/PositionOfOddAndEvenNumber.java
@@ -0,0 +1,36 @@
+/**
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于位于数组的后半部分
+ * 并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+ * <p>
+ * Created by Jiacheng on 2018/6/20.
+ */
+public class PositionOfOddAndEvenNumber {
+
+    /**
+     * 变形版冒泡
+     *
+     * @param array
+     */
+    public void reOrderArray(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        //flag用于标记当前一遍循环是否发生了交换，若没有，说明数组已经调整好顺序，返回即可
+        boolean flag = true;
+        for (int i = 0; i < array.length - 1 && flag; i++) {
+            //初始化flag
+            flag = false;
+            for (int j = array.length - 1; j > i; j--) {
+                //若是后面array[j] 为偶数，前面array[j - 1]为奇数，则交换两者
+                if ((array[j] & 1) == 1 && (array[j - 1] & 1) == 0) {
+                    array[j] = array[j] ^ array[j - 1];
+                    array[j - 1] = array[j] ^ array[j - 1];
+                    array[j] = array[j] ^ array[j - 1];
+                    //flag置true说明当前循环发生了交换
+                    flag = true;
+                }
+            }
+        }
+        return;
+    }
+}
